d56b9e52fe2b0d640f5746ea230fba0bbe37b0db
Thomas Taschauer <tom@indoo.rs>
2019-04-07 11:41:26 +0200

    add ios-cmake submodule

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ rm -rf build
 
 mkdir build/
 cd build/
-cmake -G "Xcode" -DCMAKE_TOOLCHAIN_FILE=../OpenDocument.core/lib/ios-cmake/ios.toolchain.cmake ../OpenDocument.core/
+cmake -G "Xcode" -DCMAKE_TOOLCHAIN_FILE=../ios-cmake/ios.toolchain.cmake ../OpenDocument.core/
 cd ../
