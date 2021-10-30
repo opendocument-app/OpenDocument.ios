7854fcf68307db4f71fb2fc3e5da3493dc167af2
Test <test@c02yj104jgh6.lan>
2021-10-30 17:00:01 +0200

    fix build

--- build-device.sh
+++ build-device.sh
@@ -8,5 +8,5 @@ rm -rf build
 
 mkdir build/
 cd build/
-cmake -G "Xcode" -DCMAKE_TOOLCHAIN_FILE=../ios-cmake/ios.toolchain.cmake -DIOS_DEPLOYMENT_TARGET=11.0 -DPLATFORM=OS64COMBINED ../
+cmake -G "Xcode" -DCMAKE_TOOLCHAIN_FILE=../ios-cmake/ios.toolchain.cmake -DIOS_DEPLOYMENT_TARGET=14.0 -DPLATFORM=OS64COMBINED ../
 cd ../
