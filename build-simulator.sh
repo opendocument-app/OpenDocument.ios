7da7cbf21e724c21259dc28b0c3839c7256b7850
Andreas Stefl <stefl.andreas@gmail.com>
2024-06-01 15:54:38 +0200

    reorganize

--- build-simulator.sh
+++ build-simulator.sh
@@ -4,4 +4,10 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 
-conan install . --profile ios-simulator --build missing
+conan install conan/ \
+    --output-folder=conan-build \
+    --build=missing \
+    --profile:host=conan/ios-simulator \
+    -s build_type=Release \
+    -s "&:build_type=RelWithDebInfo" \
+    -s "odrcore/*:build_type=RelWithDebInfo"
