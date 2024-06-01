7b7d726ac6ee8e461d2d06c005b5a830d7617f94
Andreas Stefl <stefl.andreas@gmail.com>
2024-06-01 18:05:49 +0200

    refactor

--- build-device.sh
+++ build-device.sh
@@ -5,9 +5,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 
 conan install conan/ \
-    --output-folder=conan-build \
+    --output-folder=conan-output \
     --build=missing \
-    --profile:host=conan/ios \
+    --profile:host=conan/profiles/ios \
     -s build_type=Release \
     -s "&:build_type=RelWithDebInfo" \
     -s "odrcore/*:build_type=RelWithDebInfo"
--- build-simulator.sh
+++ build-simulator.sh
@@ -5,9 +5,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 
 conan install conan/ \
-    --output-folder=conan-build \
+    --output-folder=conan-output \
     --build=missing \
-    --profile:host=conan/ios-simulator \
+    --profile:host=conan/profiles/ios-simulator \
     -s build_type=Release \
     -s "&:build_type=RelWithDebInfo" \
     -s "odrcore/*:build_type=RelWithDebInfo"
