afdb0131691e24e35899377d7ea3f9c06be8084b
Andreas Stefl <stefl.andreas@gmail.com>
2024-06-22 11:40:12 +0200

    hi

--- build-simulator.sh
+++ build-simulator.sh
@@ -4,13 +4,15 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 
-for configuration in "Debug" "Debug Lite" "Release" "Release Lite"; do
-  conan install conan/ \
-    --output-folder=conan-output \
-    --build=missing \
-    --profile:host=conan/profiles/ios-simulator \
-    -s build_type=Release \
-    -s "&:build_type=Release" \
-    -s "odrcore/*:build_type=RelWithDebInfo" \
-    -o "configuration=${configuration}"
+for arch in "arm64" "x64"; do
+  for configuration in "Debug" "Debug Lite" "Release" "Release Lite"; do
+    conan install conan/ \
+      --output-folder=conan-output \
+      --build=missing \
+      --profile:host=conan/profiles/ios-simulator-${arch} \
+      -s build_type=Release \
+      -s "&:build_type=Release" \
+      -s "odrcore/*:build_type=RelWithDebInfo" \
+      -o "configuration=${configuration}"
+  done
 done
