1809a4bb7cf030dedae6b6d51ee4f8758900ec03
Andreas Stefl <stefl.andreas@gmail.com>
2024-06-22 21:24:50 +0200

    patch conan files

--- conan/setup-all.sh
+++ conan/setup-all.sh
@@ -1,9 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # https://stackoverflow.com/a/246128/198996
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $DIR
+cd $DIR/..
 
+# device
+for configuration in "Debug" "Debug Lite" "Release" "Release Lite"; do
+  conan install conan/ \
+    --output-folder=conan-output \
+    --build=missing \
+    --profile:host=conan/profiles/ios \
+    -s build_type=Release \
+    -s "&:build_type=Release" \
+    -s "odrcore/*:build_type=RelWithDebInfo" \
+    -o "configuration=${configuration}"
+done
+
+# simulator
 for arch in "arm64" "x64"; do
   for configuration in "Debug" "Debug Lite" "Release" "Release Lite"; do
     conan install conan/ \
@@ -16,3 +29,5 @@ for arch in "arm64" "x64"; do
       -o "configuration=${configuration}"
   done
 done
+
+bash conan/patch.sh
