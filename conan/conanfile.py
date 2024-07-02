b428645e523639d10fc0c3bca7e40534cd9ed4e9
Andreas Stefl <stefl.andreas@gmail.com>
2024-07-02 23:16:44 +0200

    update core to 4.1.0

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -5,7 +5,7 @@ class Pkg(ConanFile):
     settings = "os", "compiler", "arch", "build_type"
     options = {"configuration": ["Debug", "Debug Lite", "Release", "Release Lite"]}
     default_options = {"configuration": "Debug"}
-    requires = "odrcore/3.0.0"
+    requires = "odrcore/4.1.0"
 
     def generate(self):
         xcode = XcodeDeps(self)
