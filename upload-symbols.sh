2791aa4e0cfcadd3fe42c51d79c02ca9aa394c87
Andreas Stefl <stefl.andreas@gmail.com>
2025-06-08 19:26:44 +0200

    chmod +x; update readme

--- upload-symbols.sh
+++ upload-symbols.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 Pods/FirebaseCrashlytics/upload-symbols -gsp OpenDocumentReader/GoogleService-Info.plist -p ios ~/Downloads/appDsyms.zip
 Pods/FirebaseCrashlytics/upload-symbols -gsp OpenDocumentReader/GoogleService-Info.plist -p ios ~/Downloads/appDsyms\ \(1\).zip
