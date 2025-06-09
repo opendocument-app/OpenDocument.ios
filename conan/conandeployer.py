877b7d102edb4d02c7fed3cd0a186d635faf0925
Andreas Stefl <stefl.andreas@gmail.com>
2025-06-09 09:36:33 +0200

    reorganize generated assets folder

--- conan/conandeployer.py
+++ conan/conandeployer.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 
 def deploy(graph, output_folder: str, **kwargs):
+    project_folder = Path(__file__).parent.parent
     output_folder = Path(output_folder).resolve()
 
     conanfile = graph.root.conanfile
@@ -22,39 +23,41 @@ def deploy(graph, output_folder: str, **kwargs):
 
     copytree_kwargs = {"symlinks": symlinks, "dirs_exist_ok": True}
 
+    conan_files = output_folder / "files"
+
     if "odrcore" in deps:
         dep = deps["odrcore"]
-        conanfile.output.info(f"Deploying odrcore to {output_folder}")
+        conanfile.output.info(f"Deploying odrcore to {conan_files}")
         shutil.copytree(
             f"{dep.package_folder}/share",
-            f"{output_folder}/odrcore",
+            f"{conan_files}/odrcore",
             **copytree_kwargs,
         )
 
     if "pdf2htmlex" in deps:
         dep = deps["pdf2htmlex"]
-        conanfile.output.info(f"Deploying pdf2htmlex to {output_folder}")
+        conanfile.output.info(f"Deploying pdf2htmlex to {conan_files}")
         shutil.copytree(
             f"{dep.package_folder}/share/pdf2htmlEX",
-            f"{output_folder}/pdf2htmlex",
+            f"{conan_files}/pdf2htmlex",
             **copytree_kwargs,
         )
 
     if "poppler-data" in deps:
         dep = deps["poppler-data"]
-        conanfile.output.info(f"Deploying poppler-data to {output_folder}")
+        conanfile.output.info(f"Deploying poppler-data to {conan_files}")
         shutil.copytree(
             f"{dep.package_folder}/share/poppler",
-            f"{output_folder}/poppler",
+            f"{conan_files}/poppler",
             **copytree_kwargs,
         )
 
     if "fontconfig" in deps:
         dep = deps["fontconfig"]
-        conanfile.output.info(f"Deploying fontconfig to {output_folder}")
+        conanfile.output.info(f"Deploying fontconfig to {conan_files}")
         shutil.copytree(
             f"{dep.package_folder}/res/share",
-            f"{output_folder}/fontconfig",
+            f"{conan_files}/fontconfig",
             **copytree_kwargs,
         )
 
@@ -62,11 +65,11 @@ def deploy(graph, output_folder: str, **kwargs):
         open(f"{output_folder}/input-files.xcfilelist", "w") as f_in,
         open(f"{output_folder}/output-files.xcfilelist", "w") as f_out,
     ):
-        for file in Path(output_folder).glob("**/*"):
-            file = file.relative_to(output_folder)
+        for file in Path(conan_files).glob("**/*"):
+            file = file.relative_to(conan_files)
             if file.suffix == ".xcfilelist":
                 continue
-            f_in.write(f"$(PROJECT_DIR)/{output_folder.name}/{file}\n")
+            f_in.write(f"$(PROJECT_DIR)/{conan_files.relative_to(project_folder)}/{file}\n")
             f_out.write(
                 f"${{TARGET_BUILD_DIR}}/${{UNLOCALIZED_RESOURCES_FOLDER_PATH}}/{file}\n"
             )
--- conan/copy-resources.sh
+++ conan/copy-resources.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
+
 set -e
 set -u
 set -o pipefail
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
 
-# copy all resources from "${PROJECT_DIR}/conan-assets" to "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
-# but ignore .xcfilelist
-rsync -av --exclude='*.xcfilelist' "${PROJECT_DIR}/conan-assets/" "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/"
+rsync -av "${PROJECT_DIR}/conan-assets/files/" "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/"
